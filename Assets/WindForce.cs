9dda1080f58e225275c0a0f2fcf5bf11c125edd8
Lajong <milos.laja.lajic@gmail.com>
2019-06-01 18:22:26 +0200

    Wind force.

---
+++ Assets/WindForce.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindForce : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * Random.Range(12,16), ForceMode.Impulse);
+    }
+}
