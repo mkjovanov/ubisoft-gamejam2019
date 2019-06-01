92a8571d5641fced9aaafee27b0d7ceb9b72ec35
Lajong <milos.laja.lajic@gmail.com>
2019-06-01 15:22:07 +0200

    -Base unit, movement script.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,12 +7,9 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    void FixedUpdate()
     {
-        
     }
 }
