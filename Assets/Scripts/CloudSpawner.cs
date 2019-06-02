d181e3fbd99d38443a4d04e1898667e9888341be
Marko Jovanov <mkjovanov@gmail.com>
2019-06-02 07:44:45 +0200

    Fixed failing build

--- Assets/Scripts/CloudSpawner.cs
+++ Assets/Scripts/CloudSpawner.cs
@@ -9,7 +9,7 @@ public class CloudSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(0) &&  && GameManager.Instance.IsRainyCloudActivated)
+        if (Input.GetKeyDown(0) && GameManager.Instance.IsRainyCloudActivated)
         {
             SpawnCloud();
         }
