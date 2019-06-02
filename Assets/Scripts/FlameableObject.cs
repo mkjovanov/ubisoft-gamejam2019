861ddc02489e80f172aa1228ef93464f81e03821
Lajong <milos.laja.lajic@gmail.com>
2019-06-02 03:38:28 +0200

    Level 1 update.

--- Assets/Scripts/FlameableObject.cs
+++ Assets/Scripts/FlameableObject.cs
@@ -4,12 +4,19 @@ public class FlameableObject : MonoBehaviour
 {
     public float BurningTime = 2f;
     public bool IsOnFire = false;
+    public GameObject FireObject;
+    private bool firesSpawned = false;
     private float _colorChange = 0;
 
     void Update()
     {
         if (IsOnFire)
         {
+            if (!firesSpawned)
+            {
+                Instantiate(FireObject, transform.position, Quaternion.identity);
+                firesSpawned = true;
+            }
             Renderer rend = GetComponent<Renderer>();
             BurningTime -= Time.deltaTime;
             if (_colorChange < 1)
@@ -17,10 +24,12 @@ public class FlameableObject : MonoBehaviour
                 _colorChange += Time.deltaTime / BurningTime;
             }
 
-            rend.material.shader = Shader.Find("_Color");
-            rend.material.color = Color.Lerp(Color.green, Color.red, _colorChange);
-            rend.material.shader = Shader.Find("Specular");
-            rend.material.color = Color.Lerp(Color.green, Color.red, _colorChange);
+            //rend.material.shader = Shader.Find("_Color");
+            //rend.material.color = Color.Lerp(Color.green, Color.red, _colorChange);
+            //rend.material.shader = Shader.Find("Specular");
+            //rend.material.color = Color.Lerp(Color.green, Color.red, _colorChange);
+
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z * .99f);
 
             if (BurningTime <= 0)
             {
